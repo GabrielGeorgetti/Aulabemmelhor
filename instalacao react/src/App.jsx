b55d47514e4cb0b7e22f51a61679a7ae309fb666
GabrielGeorgetti <georgettisilvag@gmail.com>
2026-02-23 22:45:41 -0300

    first commit

---
+++ instalacao react/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Olá mundo!</h1>
+      <p>Essa é minha página React</p>
+    </div>
+  )
+}
+
+export default App
---
+++ instalacao react/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig, ModuleNode } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
